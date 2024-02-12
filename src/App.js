6c90b1ea16bc29677d264bded9f44634462a3a3c
navami-dataequinox <navami@datequinox.com>
2024-02-12 11:42:20 +0530

    [CN-01] Feat-Implemented counter functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Counter from './Counter';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+<Counter/>
+    </>
   );
 }
 
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import { Button, Stack } from '@mui/material'
+import React, { useState } from 'react'
 
- export function Counter(){
-    return(
+function Counter() {
+    const [counter,setCounter] = useState(0)
 
-    )
+    const handleIncrement= ()=>{
+setCounter(counter+1)
+    }
+    const handleDecrement=()=>{
+        if(counter>0){
+            setCounter(counter-1)
+
+        }
+            }
+  return (
+   <>
+   <Stack direction='row' justifyContent='center' sx={{marginTop:'90px',fontSize:'45px'}}>{counter}</Stack>
+    <Stack direction='row' spacing={2} justifyContent='center' sx={{marginTop:'80px'}}>
+    <Button onClick={handleIncrement} variant='contained'>Increment</Button>
+    <Button onClick={handleDecrement} variant='contained'>Decrement</Button>
+    </Stack>
+   
+
+   </> 
+  )
 }
+
+export default Counter
 
